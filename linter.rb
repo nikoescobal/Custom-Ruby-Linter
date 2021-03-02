1a48893c149deb32aa53f65b735838f3413fd499
Niko Escobal <niko.escobal@gmail.com>
2021-03-02 18:17:34 +0800

    Added run_check method.

--- linter.rb
+++ linter.rb
@@ -5,3 +5,16 @@ class Linter
   def initialize()
     # @url = url
   end
+
+  def run_check
+    File.open("style.css", "r") do |file|
+      i = 0
+      for line in file.readlines()
+        i += 1
+        if line.include? "#"
+          hex_case(line, i) 
+        end
+        trailing_spaces(line, i)
+      end
+    end
+  end
