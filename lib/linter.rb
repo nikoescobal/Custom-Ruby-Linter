474516e919736845de7f4642a35d0ee80b8098b6
Niko Escobal <niko.escobal@gmail.com>
2021-03-04 21:19:59 +0800

    Fixed rubocop errors.

--- lib/linter.rb
+++ lib/linter.rb
@@ -1,5 +1,6 @@
 class Linter
   attr_reader :url, :errors
+
   def initialize(url)
     @url = url
     @errors = []
@@ -9,56 +10,47 @@ class Linter
     i = 0
     File.foreach(@url) do |line|
       i += 1
-      # check_hex_case(line, i)
-      # check_trailing_spaces(line, i)
-      # check_braces(line, i)
-      # check_semicolons(line, i)
-      # check_decimals(line, i)
-      # check_font_weight(line, i)
-      # check_comma_spaces(line, i)
+      check_hex_case(line, i)
+      check_trailing_spaces(line, i)
+      check_braces(line, i)
+      check_semicolons(line, i)
+      check_decimals(line, i)
+      check_font_weight(line, i)
+      check_comma_spaces(line, i)
       check_leading_zeros(line, i)
     end
-    return @errors.sort{|el1, el2| el2 <=> el1}
+    @errors.sort { |el1, el2| el2 <=> el1 }
   end
 
   private
 
   def check_hex_case(line, number)
-    if line.include?("#")
-      hex = line.split("#")[1].gsub(/\W/,"")
-      if hex == hex.upcase
-        @errors << "##{hex} should be lowercase on line #{number}."
-      end
-    end
+    return unless line.include?('#')
+
+    hex = line.split('#')[1].gsub(/\W/, '')
+    @errors << "##{hex} should be lowercase on line #{number}." if hex == hex.upcase
   end
 
   def check_trailing_spaces(line, number)
     stripped_line = line.delete("\n")
-    if stripped_line.end_with?(" ")
-      @errors << "Trailing spaces detected on line #{number}."
-    end
+    @errors << "Trailing spaces detected on line #{number}." if stripped_line.end_with?(' ')
   end
 
   def check_braces(line, number)
-    if line.include?("{") && line.include?("}")
-      @errors << "Two braces detected on line #{number}."
-    end
+    @errors << "Two braces detected on line #{number}." if line.include?('{') && line.include?('}')
   end
 
-  def check_semicolons (line, number)
-    if line.include?(";;")
-      @errors << "Extra semicolon detected on line #{number}."
-    end
+  def check_semicolons(line, number)
+    @errors << "Extra semicolon detected on line #{number}." if line.include?(';;')
   end
 
   def check_decimals(line, number)
-    if line.include?(':')
+    return unless line.include?(':')
       val = line.split(':')[1]
       decimals = val.gsub(/[^\d,.]/i, '')
-      if decimals.include?(".") && decimals.split(".")[1].length > 2
+      if decimals.include?('.') && decimals.split('.')[1].length > 2
         @errors << "Max decimal places should be limited to 2 on line #{number}."
       end
-    end
   end
 
   def check_font_weight(line, number)
@@ -66,21 +58,15 @@ class Linter
   end
 
   def check_comma_spaces(line, number)
-    if line.include?(',')
+    return unless line.include?(',')
       val = line.split(',')[1]
-      if val[0] != " "
-        @errors << "There should be a space after the comma on line #{number}."
-      end
+      @errors << "There should be a space after the comma on line #{number}." if val[0] != ' '
     end
-  end
 
   def check_leading_zeros(line, number)
-    if line.include?(":")
-      val = line.split(":")[1]
+    return unless line.include?(':')
+      val = line.split(':')[1]
       decimals = val.gsub(/[^\d,.]/i, '')
-      if decimals[0] == "."
-        @errors << "There should be a zero before the decimal point on line #{number}."
-      end
+      @errors << "There should be a zero before the decimal point on line #{number}." if decimals[0] == '.'
     end
-  end
 end
