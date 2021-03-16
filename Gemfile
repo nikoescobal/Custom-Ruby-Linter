637b8a7abe5e7c180767d83df1bc480056a75251
Niko Escobal <niko.escobal@gmail.com>
2021-03-16 20:18:44 +0800

    Added Gemfile, color functionality, and  updated bundler version.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rubocop'
+gem 'rspec'
+gem 'colorize'
--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/linter'
 
-linter = Linter.new('./bin/style.css')
+linter = Linter.new('../bin/style.css')
 puts linter.run_check
